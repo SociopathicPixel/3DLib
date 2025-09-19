f66090581831fbbd9e3f8f41b3a7ea9a90a5498b
Pixel <38221979+SociopathicPixel@users.noreply.github.com>
2025-09-19 11:39:02 +0200

    Revert "Dev/project setup"

--- src/main/java/nl/pixel/printlib/machine/repository/MachineRepository.java
+++
@@ -1,9 +0,0 @@
-package nl.pixel.printlib.machine.repository;
-
-import nl.pixel.printlib.machine.entity.MachineEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MachineRepository extends JpaRepository<MachineEntity, Long> {
-}
