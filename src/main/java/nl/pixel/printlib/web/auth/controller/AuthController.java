84b88febd02245ff860d7bb4e779fce271a5d00a
Jan Bob <janbob_prive@outlook.com>
2025-09-23 10:45:07 +0200

    updating code due to review comments

--- src/main/java/nl/pixel/printlib/web/auth/controller/AuthController.java
+++ src/main/java/nl/pixel/printlib/web/auth/controller/AuthController.java
@@ -2,17 +2,13 @@ package nl.pixel.printlib.web.auth.controller;
 
 import nl.pixel.printlib.domain.model.user.entity.User;
 import nl.pixel.printlib.domain.model.user.exception.UserRegistrationException;
-import nl.pixel.printlib.domain.model.user.repository.UserRepository;
 import nl.pixel.printlib.domain.model.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.io.IOException;
 
 @Controller
 public class AuthController {
