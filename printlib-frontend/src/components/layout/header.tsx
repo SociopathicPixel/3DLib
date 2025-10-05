cee9b101a91ce9969395cbd704df115d0f6784a1
Pixel <janbob_prive@outlook.com>
2025-10-05 16:33:24 +0200

    Add my devices icon and update header navigation
    Added a new my_devices.png asset and updated header.tsx to include a 'My devices' button. All header icon buttons now have onClick handlers for navigation, improving UI consistency.

--- printlib-frontend/src/components/layout/header.tsx
+++ printlib-frontend/src/components/layout/header.tsx
@@ -7,49 +7,32 @@ const Header: React.FC = () => {
     <header className={styles.header}>
       <div className={styles.leftGroup}>
         <img src="./assets/app_logo.png" alt="Logo" className={styles.logo} />
-        <button className={styles.iconButton}>
+        <button className={styles.iconButton} onClick={() => {"/explore"}}>
           <img
             src="./assets/explore_models.png"
             alt="Explore Models"
             className={styles.menu}
           />
         </button>
-        <button className={styles.iconButton}>
-          <img
-            src="./assets/my_collection.png"
-            alt="My collection"
-            className={styles.menu}
-          />
+        <button className={styles.iconButton} onClick={() => {"/collection"}}>
+          <img src="./assets/my_collection.png" alt="My collection" className={styles.menu}/>
         </button>
-        <button className={styles.iconButton}>
-          <img
-            src="./assets/models_add.png"
-            alt="Add Model"
-            className={styles.menu}
-          />
+        <button className={styles.iconButton} onClick={() => {"/add-model"}}>
+          <img src="./assets/models_add.png" alt="Add Model" className={styles.menu}/>
         </button>
-        <button className={styles.iconButton}>
-          <img
-            src="./assets/filament_add.png"
-            alt="Add filament"
-            className={styles.menu}
-          />
+        <button className={styles.iconButton} onClick={() => {"/add-filament"}}>
+          <img src="./assets/filament_add.png" alt="Add filament" className={styles.menu}/>
+        </button>
+        <button className={styles.iconButton} onClick={() => {"/my-devices"}}>
+          <img src="./assets/my_devices.png" alt="My devices" className={styles.menu} />
         </button>
       </div>
       <div className={styles.rightGroup}>
-        <button className={styles.iconButton}>
-          <img
-            src="./assets/user_profile.png"
-            alt="User profile"
-            className={styles.menu}
-          />
+        <button className={styles.iconButton} onClick={() => {"/profile"}}>
+          <img src="./assets/user_profile.png" alt="User profile" className={styles.menu}/>
         </button>
-        <button className={styles.iconButton}>
-          <img
-            src="./assets/app_settings.png"
-            alt="App settings"
-            className={styles.menu}
-          />
+        <button className={styles.iconButton} onClick={() => {"/settings"}}>
+          <img src="./assets/app_settings.png" alt="App settings" className={styles.menu}/>
         </button>
       </div>
     </header>
