162515c54c2e5bc0337b36117b8297ad08436afb
Pixel <janbob_prive@outlook.com>
2025-10-05 17:21:13 +0200

    Add search bar to header on landing page
    Introduced a search bar in the header that appears only on the landing page. Updated header layout and styles to support the new search bar and conditionally render navigation buttons based on the current route.

--- printlib-frontend/src/components/layout/header.tsx
+++ printlib-frontend/src/components/layout/header.tsx
@@ -1,30 +1,41 @@
 import React from "react";
 import styles from "./header.module.scss";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const Header: React.FC = () => {  
   const navigate = useNavigate();
+  const location = useLocation();
+  const isLandingPage = location.pathname === "/landing";
 
   return (
     <header className={styles.header}>
       <div className={styles.leftGroup}>
         <img src="./assets/app_logo.png" alt="Logo" className={styles.logo} />
-        <button className={styles.iconButton} onClick={() => navigate("/explore")}>
-          <img src="./assets/explore_models.png" alt="Explore Models" className={styles.menu}/>
-        </button>
-        <button className={styles.iconButton} onClick={() => navigate("/collection")}>
-          <img src="./assets/my_collection.png" alt="My collection" className={styles.menu}/>
-        </button>
-        <button className={styles.iconButton} onClick={() => navigate("/add-model")}>
-          <img src="./assets/models_add.png" alt="Add Model" className={styles.menu}/>
-        </button>
-        <button className={styles.iconButton} onClick={() => navigate("/add-filament")}>
-          <img src="./assets/filament_add.png" alt="Add filament" className={styles.menu}/>
-        </button>
-        <button className={styles.iconButton} onClick={() => navigate("/my-devices")}>
-          <img src="./assets/my_devices.png" alt="My devices" className={styles.menu} />
-        </button>
+        {!isLandingPage ? (
+          <div>
+            <button className={styles.iconButton} onClick={() => navigate("/explore")}>
+              <img src="./assets/explore_models.png" alt="Explore Models" className={styles.menu}/>
+            </button>
+            <button className={styles.iconButton} onClick={() => navigate("/collection")}>
+              <img src="./assets/my_collection.png" alt="My collection" className={styles.menu}/>
+            </button>
+            <button className={styles.iconButton} onClick={() => navigate("/add-model")}>
+              <img src="./assets/models_add.png" alt="Add Model" className={styles.menu}/>
+            </button>
+            <button className={styles.iconButton} onClick={() => navigate("/add-filament")}>
+              <img src="./assets/filament_add.png" alt="Add filament" className={styles.menu}/>
+            </button>
+            <button className={styles.iconButton} onClick={() => navigate("/my-devices")}>
+              <img src="./assets/my_devices.png" alt="My devices" className={styles.menu} />
+            </button>
+          </div>
+        ) : ( <></>)}
       </div>
+      {isLandingPage ? (
+        <div className={styles.middleGroup}>
+          <input type="text" placeholder="Search..." className={styles.searchBar} />
+        </div>
+      ): ( <></>)}
       <div className={styles.rightGroup}>
         <button className={styles.iconButton} onClick={() => navigate("/profile")}>
           <img src="./assets/user_profile.png" alt="User profile" className={styles.menu}/>
